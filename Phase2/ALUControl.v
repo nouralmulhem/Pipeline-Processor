8ce27ab8fcb0ef9f623936fe2f10c2104d8191cd
BasmaElhoseny <basmaelhoseny6@gmail.com>
2022-12-26 13:38:34 +0200

    Added New CU

--- Phase2/ALUControl.v
+++ Phase2/ALUControl.v
@@ -1,13 +1,46 @@
 //ALU Control for phase 1 should be modified later
-module ALUControl (ALUOp, Funct, Operation);
-    input ALUOp;
-    input [2:0] Funct;
-    output [2:0] Operation;
+module ALUControl (aluOp, funct, opcode, aluControl);
+    input aluOp;
+    input [3:0] opcode
+    input [2:0] funct;
+    output [2:0] aluControl;
 
     // if combinational circuit
-    assign Operation = 
-        (ALUOp == 1) ? ((Funct == 3'b001) ? 3'b001:                     //LDD
-                        (Funct == 3'b111) ? 3'b111:                     //LDM
-                        (Funct == 3'b010) ? 3'b010:                     //Store
-                        (Funct == 3'b011) ? 3'b011: 3'b100) : 3'b101;   // Add : Not : Nop(ALUOp is 0) 
+    // assign aluControl = 
+    //     (aluOp == 1) ? ((funct == 3'b001) ? 3'b001:                     //LDD
+    //                     (funct == 3'b111) ? 3'b111:                     //LDM
+    //                     (funct == 3'b010) ? 3'b010:                     //Store
+    //                     (funct == 3'b011) ? 3'b011: 3'b100) : 3'b101;   // Add : Not : Nop(aluOp is 0) 
+
+    assign aluControl = (aluOp == 1) ?
+        (opcode == 4'b0001 && funct = 3'b) ? 3'b :  // clrc
+        (opcode == 4'b0001 && funct = 3'b) ? 3'b :  // setc
+        (opcode == 4'b0010 && funct = 3'b) ? 3'b :  // not
+        (opcode == 4'b0010 && funct = 3'b) ? 3'b :  // inc
+        (opcode == 4'b0010 && funct = 3'b) ? 3'b :  // dec
+        (opcode == 4'b0010 && funct = 3'b) ? 3'b :  // mov
+        (opcode == 4'b0010 && funct = 3'b) ? 3'b :  // add
+        (opcode == 4'b0010 && funct = 3'b) ? 3'b :  // sub
+        (opcode == 4'b0010 && funct = 3'b) ? 3'b :  // and
+        (opcode == 4'b0010 && funct = 3'b) ? 3'b :  // or
+        (opcode == 4'b0101 && funct = 3'b) ? 3'b :  // shl
+        (opcode == 4'b0101 && funct = 3'b) ? 3'b :  // shr
+        (opcode == 4'b0101 && funct = 3'b) ? 3'b :  // ldm
+        (opcode == 4'b1011 && funct = 3'b) ? 3'b :  // ldd
+        (opcode == 4'b1100 && funct = 3'b) ? 3'b :  // std
+        (opcode == 4'b1100 && funct = 3'b) ? 3'b :  // push
+        (opcode == 4'b1100 && funct = 3'b) ? 3'b :  // pop
+        (opcode == 4'b1000 && funct = 3'b) ? 3'b :  // jz
+        (opcode == 4'b1000 && funct = 3'b) ? 3'b :  // jn
+        (opcode == 4'b1000 && funct = 3'b) ? 3'b :  // jc
+        (opcode == 4'b1000 && funct = 3'b) ? 3'b :  // jmp
+        (opcode == 4'b1001 && funct = 3'b) ? 3'b :  // call
+        (opcode == 4'b1010 && funct = 3'b) ? 3'b :  // rti
+        (opcode == 4'b1010 && funct = 3'b) ? 3'b :  // ret
+        (opcode == 4'b0011 && funct = 3'b) ? 3'b :  // out
+        (opcode == 4'b0100 && funct = 3'b) ? 3'b :  // in : default: nop
+
+
+
+
 endmodule
--- Phase2/Controle_Signal.v
+++ Phase2/Controle_Signal.v
@@ -3,137 +3,235 @@
 /*
 Controle Unit which send controle signals 
 inputs:
-  - opcode
+  - opcode(4 bits):Opcode
 output:
-  - all output signals
   -ALU OP
   -ALU src
-  -MEMW
-  -MEMR
-  -Memory to Reg(MTR)
   -reg write
-
+  -MEMR
+  -MEMW
+  -Memory to Reg(MTR)
   -Branch
-  -In
   -Out
-  -Stack op
-  -Push
+  -In
+  -PushPop
+  -pushPc
+  -popPc
+  -spop
+  // -flush
 */
 module CU(
 opcode,
+interrupt,
 ALU_OP,
 ALU_src,
-MEMW,
-MEMR,
-MTR,
 reg_write,
+MEMR,
+MEMW,
+MTR,
 Branch,
-In,
 Out,
-Stack_op,
-Push
+In,
+PushPop,
+PushPc,
+PopPc,
+Spop
+// Flush
 );
-input [2:0] opcode;
+input [3:0] opcode;
+input interrupt;
+output ALU_OP,
+ALU_src,
+reg_write,
+MEMR,
+MEMW,
+MTR,
+Branch,
+Out,
+In,
+PushPop,
+PushPc,
+PopPc,
+Spop;
+// Flush;
 
-output reg ALU_OP,ALU_src,MEMW,MEMR,MTR,reg_write,Branch,In,Out,Stack_op,Push;
+//1=>smame as call
+assign ALU_OP=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+              (opcode == 4'b0001) ?1'b1://CLRC-SETC
+              (opcode == 4'b0010)?1'b1://NOT
+              (opcode == 4'b0011)?1'b1://INC
+              (opcode == 4'b0100)?1'b1://DEC
+              (opcode == 4'b0101)?1'b1://OUT
+              (opcode == 4'b0110)?1'b1://IN
+              (opcode == 4'b0111)?1'b1://
+              (opcode == 4'b1000)?1'b0://
+              (opcode == 4'b1001)?1'b0://
+              (opcode == 4'b1010)?1'b0:1'b0;//
 
-always @ (*) begin
-  if(opcode == 3'b001) begin   
-//LDD
-    ALU_OP=1'b1;
-    ALU_src=1'b0;
-    MEMW=1'b0;
-    MEMR=1'b1;
-    MTR=1'b1;
-    reg_write=1'b1;
 
-    Branch=1'b0;
-    In=1'b0;
-    Out=1'b0;
-    Stack_op=1'b0;
-    Push=1'b0;
+assign ALU_src=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+               (opcode == 4'b0001) ?1'b0://CLRC-SETC
+               (opcode == 4'b0010)?1'b0://NOT
+               (opcode == 4'b0011)?1'b0://INC
+               (opcode == 4'b0100)?1'b0://DEC
+               (opcode == 4'b0101)?1'b1://OUT
+               (opcode == 4'b0110)?1'b0://IN
+               (opcode == 4'b0111)?1'b0://
+               (opcode == 4'b1000)?1'b0://
+               (opcode == 4'b1001)?1'b0://
+               (opcode == 4'b1010)?1'b0:1'b0;//
 
-  end
-  else if(opcode == 3'b111) begin
-  //LDM
-    ALU_OP=1'b1;
-    ALU_src=1'b1;
-    MEMW=1'b0;
-    MEMR=1'b0;
-    MTR=1'b0;
-    reg_write=1'b1;
 
-    Branch=1'b0;
-    In=1'b0;
-    Out=1'b0;
-    Stack_op=1'b0;
-    Push=1'b0;
+assign reg_write=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b1://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b1://DEC
+                 (opcode == 4'b0101)?1'b1://OUT
+                 (opcode == 4'b0110)?1'b0://IN
+                 (opcode == 4'b0111)?1'b1://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b0://
+                 (opcode == 4'b1010)?1'b0:1'b0;//
 
-  end
-  else if(opcode == 3'b010) begin
-//STD
-    ALU_OP=1'b1;
-    ALU_src=1'b0;
-    MEMW=1'b1;
-    MEMR=1'b0;
-    MTR=1'b0;
-    reg_write=1'b0;
 
-    Branch=1'b0;
-    In=1'b0;
-    Out=1'b0;
-    Stack_op=1'b0;
-    Push=1'b0;
+assign MEMR=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b0://IN
+                 (opcode == 4'b0111)?1'b1://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b0://
+                 (opcode == 4'b1010)?1'b1:1'b0;//
 
-  end  
-  else if(opcode == 3'b011) begin
-//ADD
-    ALU_OP=1'b1;
-    ALU_src=1'b0;
-    MEMW=1'b0;
-    MEMR=1'b0;
-    MTR=1'b0;
-    reg_write=1'b1;
 
-    Branch=1'b0;
-    In=1'b0;
-    Out=1'b0;
-    Stack_op=1'b0;
-    Push=1'b0;
+assign MEMW=(interrupt==1'b1)?1'b1:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b1://IN
+                 (opcode == 4'b0111)?1'b0://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b1://
+                 (opcode == 4'b1010)?1'b0:1'b0;//
 
-  end
-  else if(opcode == 3'b100) begin
-//NOT
-    ALU_OP=1'b1;
-    ALU_src=1'b0;
-    MEMW=1'b0;
-    MEMR=1'b0;
-    MTR=1'b0;
-    reg_write=1'b1;
+                 
+assign MTR=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b0://IN
+                 (opcode == 4'b0111)?1'b1://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b0://
+                 (opcode == 4'b1010)?1'b0:1'b0;//
 
-    Branch=1'b0;
-    In=1'b0;
-    Out=1'b0;
-    Stack_op=1'b0;
-    Push=1'b0;
 
-  end
-  else if(opcode == 3'b101) begin
-//NOP
-    ALU_OP=1'b0;
-    ALU_src=1'b0;
-    MEMW=1'b0;
-    MEMR=1'b0;
-    MTR=1'b0;
-    reg_write=1'b0;
+assign Branch=(interrupt==1'b1)?1'b1:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b0://IN
+                 (opcode == 4'b0111)?1'b0://
+                 (opcode == 4'b1000)?1'b1://
+                 (opcode == 4'b1001)?1'b1://
+                 (opcode == 4'b1010)?1'b0:1'b0;//
 
-    Branch=1'b0;
-    In=1'b0;
-    Out=1'b0;
-    Stack_op=1'b0;
-    Push=1'b0;
-  end
 
-end
+                 
+assign Out=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b1://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b0://IN
+                 (opcode == 4'b0111)?1'b0://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b0://
+                 (opcode == 4'b1010)?1'b0:1'b0;//
+
+assign In=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b1://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b0://IN
+                 (opcode == 4'b0111)?1'b0://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b0://
+                 (opcode == 4'b1010)?1'b0:1'b0;//
+
+
+assign PushPop=(interrupt==1'b1)?1'b1:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b1://IN
+                 (opcode == 4'b0111)?1'b0://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b1://
+                 (opcode == 4'b1010)?1'b0:1'b0;//   
+
+
+assign PushPc=(interrupt==1'b1)?1'b1:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b0://IN
+                 (opcode == 4'b0111)?1'b0://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b1://
+                 (opcode == 4'b1010)?1'b0:1'b0;//       
+                 
+ assign PopPc=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b0://IN
+                 (opcode == 4'b0111)?1'b0://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b0://
+                 (opcode == 4'b1010)?1'b1:1'b0;// 
+
+assign Spop=(interrupt==1'b1)?1'b1:(opcode == 4'b0000) ?1'b0://NOP
+                 (opcode == 4'b0001) ?1'b0://CLRC-SETC
+                 (opcode == 4'b0010)?1'b0://NOT
+                 (opcode == 4'b0011)?1'b0://INC
+                 (opcode == 4'b0100)?1'b0://DEC
+                 (opcode == 4'b0101)?1'b0://OUT
+                 (opcode == 4'b0110)?1'b1://IN
+                 (opcode == 4'b0111)?1'b1://
+                 (opcode == 4'b1000)?1'b0://
+                 (opcode == 4'b1001)?1'b1://
+                 (opcode == 4'b1010)?1'b1:1'b0;//     
+
+// assign Flush=(interrupt==1'b1)?1'b0:(opcode == 4'b0000) ?1'b0://NOP
+//                  (opcode == 4'b0001) ?1'b0://CLRC-SETC
+//                  (opcode == 4'b0010)?1'b0://NOT
+//                  (opcode == 4'b0011)?1'b0://INC
+//                  (opcode == 4'b0100)?1'b0://DEC
+//                  (opcode == 4'b0101)?1'b0://OUT
+//                  (opcode == 4'b0110)?1'b0://IN
+//                  (opcode == 4'b0111)?1'b0://
+//                  (opcode == 4'b1000)?1'b0://
+//                  (opcode == 4'b1001)?1'b0://
+//                  (opcode == 4'b1010)?1'b1:1'b0;//                             
 
 endmodule
