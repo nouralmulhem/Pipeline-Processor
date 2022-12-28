abd2448e9eb0c8bf301cc0789a5c6cf81b4ab326
Yasmine Ghanem <74925701+yasmineghanem@users.noreply.github.com>
2022-12-29 01:57:43 +0200

    forwarding unit done :)

--- Phase2/DE_Buffer.v
+++ Phase2/DE_Buffer.v
@@ -12,26 +12,27 @@ output:
   -controlSignals_out (8 bits): control signals from the Control Unit
   -readData1_out (16 bits)
   -readData2_out (16 bits)
-  -writeAdd_out (3 bits)
+  -writeAdd_out1 (3 bits)
   -function_out (3 bits)   
 */
-module DE_Buffer(clk, controlSignals_in, readData1_in, readData2_in, writeAdd_in, function_in, controlSignals_out, readData1_out, readData2_out, writeAdd_out, function_out);
+module DE_Buffer(clk, controlSignals_in, readData1_in, readData2_in, writeAdd_in1, writeAdd_in2, function_in, controlSignals_out, readData1_out, readData2_out, writeAdd_out1, writeAdd_out2,function_out);
     input [12:0] controlSignals_in;
     input [15:0] readData1_in, readData2_in;
-    input [2:0] writeAdd_in;
+    input [2:0] writeAdd_in1, writeAdd_in2;
     input [3:0] function_in;
     input clk;
 
     output reg [12:0] controlSignals_out;
     output reg [15:0] readData1_out, readData2_out;
-    output reg [2:0] writeAdd_out;
+    output reg [2:0] writeAdd_out1, writeAdd_out2;
     output reg [3:0] function_out;
 
     always @(posedge clk) begin
         controlSignals_out = controlSignals_in;
         readData1_out = readData1_in;
         readData2_out = readData2_in;
-        writeAdd_out = writeAdd_in;
+        writeAdd_out1 = writeAdd_in1;
+        writeAdd_out2 = writeAdd_in2;
         function_out = function_in;
     end
 endmodule
